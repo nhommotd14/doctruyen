6ee8dc3365c43fea30085e0636a61332b704c58f
tranquanghuy341 <tranquanghuy341@gmail.com>
2018-03-18 23:57:11 +0700

    add favourite activity

--- Project/app/src/main/java/com/nhommot/doctruyen/ui/ChaptersActivity.java
+++ Project/app/src/main/java/com/nhommot/doctruyen/ui/ChaptersActivity.java
@@ -1,20 +1,22 @@
-package com.nhommot.doctruyen;
+package com.nhommot.doctruyen.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.nhommot.doctruyen.R;
+
 import java.util.ArrayList;
 
-public class ItemChapters extends AppCompatActivity {
+public class ChaptersActivity extends AppCompatActivity {
     private ArrayList<String> data = new ArrayList<>();
     private ListView lvChapters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_chapters);
+        setContentView(R.layout.activity_chapters);
         setControl();
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, data);
         lvChapters.setAdapter(adapter);
