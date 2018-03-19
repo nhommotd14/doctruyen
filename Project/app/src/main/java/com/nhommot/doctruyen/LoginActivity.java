52e60ffaa128cf574e0f71622967cb4cd2043885
Nguyen Tan Dung <tandung847@gmail.com>
2018-03-19 13:37:36 +0700

    change package from Toan's layout

--- Project/app/src/main/java/com/nhommot/doctruyen/LoginActivity.java
+++ Project/app/src/main/java/com/nhommot/doctruyen/LoginActivity.java
@@ -1,10 +1,12 @@
-package com.example.toanld.loginform;
+package com.nhommot.doctruyen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.nhommot.doctruyen.R;
 
 public class LoginActivity extends AppCompatActivity {
     Button btn_login;
