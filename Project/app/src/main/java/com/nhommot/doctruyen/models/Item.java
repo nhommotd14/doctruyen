561d08dc19c9c98bb730723a040b36b582298714
tranquanghuy341 <tranquanghuy341@gmail.com>
2018-04-08 16:12:41 +0700

    test add value to firebase

---
+++ Project/app/src/main/java/com/nhommot/doctruyen/models/Item.java
@@ -0,0 +1,88 @@
+package com.nhommot.doctruyen.models;
+
+/**
+ * Created by Huy on 4/8/2018.
+ */
+
+public class Item {
+
+    private int itemId;
+    private String author;
+    private String name;
+    private String description;
+    private String imgPreview;
+    private int typeId;
+    private int views;
+
+    public Item(){
+
+    }
+
+    public Item(int itemId, String author, String name, String description, String imgPreview, int typeId, int views) {
+        this.itemId = itemId;
+        this.author = author;
+        this.name = name;
+        this.description = description;
+        this.imgPreview = imgPreview;
+        this.typeId = typeId;
+        this.views = views;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgPreview() {
+        return imgPreview;
+    }
+
+    public void setImgPreview(String imgPreview) {
+        this.imgPreview = imgPreview;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+
+}
---
+++ Project/app/src/main/java/com/nhommot/doctruyen/ui/MainActivity.java
@@ -0,0 +1,69 @@
+package com.nhommot.doctruyen.ui;
+
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nhommot.doctruyen.R;
+import com.nhommot.doctruyen.models.Item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        test();
+    }
+
+    public void test(){
+        Log.d(TAG, "test: ");
+        Item item1 = new Item();
+        item1.setName("Batman1");
+        DatabaseReference mData = FirebaseDatabase.getInstance().getReference();
+        mData.child("Item").push().setValue(item1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
