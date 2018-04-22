549c58218c748b7fc2e4dea0cd594dff1ac1944d
tranquanghuy341 <tranquanghuy341@gmail.com>
2018-04-22 16:42:42 +0700

    update chpater

--- Project/app/src/main/java/com/nhommot/doctruyen/ui/fragments/ChapterFragment.java
+++ Project/app/src/main/java/com/nhommot/doctruyen/ui/fragments/ChapterFragment.java
@@ -3,16 +3,28 @@ package com.nhommot.doctruyen.ui.fragments;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.nhommot.doctruyen.R;
 import com.nhommot.doctruyen.models.Book;
 import com.nhommot.doctruyen.models.Chapter;
+import com.nhommot.doctruyen.models.User;
 import com.nhommot.doctruyen.ui.adapters.ChapterAdapter;
+import com.nhommot.doctruyen.ui.adapters.SimpleDividerItemDecoration;
+import com.nhommot.doctruyen.utils.FirebaseUtils;
 import com.nhommot.doctruyen.utils.SharedPrefsUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Huy on 4/12/2018.
@@ -21,19 +33,60 @@ import com.nhommot.doctruyen.utils.SharedPrefsUtils;
 public class ChapterFragment extends Fragment {
     private final String TAG = "ChaptperFragment";
     private ChapterAdapter mAdapter;
-    private String bookId;
-    private ListView listChapters;
+
+    List<Chapter> result;
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_chapters, container, false);
-        listChapters = rootView.findViewById(R.id.listChapters);
+
 //        Get current book id
         String currentBookId = SharedPrefsUtils.getCurrentBookId(this.getContext());
         if (currentBookId != null){
+            result = new ArrayList<>();
+            recyclerView = rootView.findViewById(R.id.chapter_recycler_view);
+            recyclerView.setHasFixedSize(true);
+            LinearLayoutManager llm = new LinearLayoutManager(this.getContext());
+            llm.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(llm);
+            recyclerView.addItemDecoration(new SimpleDividerItemDecoration(this.getContext()));
+            Log.d(TAG, "onCreate: 1=======================");
+            mAdapter = new ChapterAdapter(result);
+            Log.d(TAG, "onCreate: 2=======================");
+            recyclerView.setAdapter(mAdapter);
+            Log.d(TAG, "onCreate: 3=======================");
+            FirebaseUtils.getChapterRef().child(currentBookId).addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Log.d(TAG, "onChildAdded: KEY " + dataSnapshot.getKey());
+                    Chapter chapter = dataSnapshot.getValue(Chapter.class);
+                    result.add(chapter);
+                    mAdapter.notifyDataSetChanged();
+                }
 
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
 
         return rootView;
