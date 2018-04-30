1baf16a87342e17cd7d06a8511ea6d408f03ca1d
falconxi1611 <ductoanle1611@gmail.com>
2018-04-30 14:51:05 +0700

    update login activity

--- Project/app/src/main/java/com/nhommot/doctruyen/models/User.java
+++ Project/app/src/main/java/com/nhommot/doctruyen/models/User.java
@@ -1,89 +1,22 @@
 package com.nhommot.doctruyen.models;
 
 /**
- * Created by Huy on 4/11/2018.
+ * Edited by Toan on 4/30/2018.
  */
 
 public class User {
-    private String userId;
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String createDate;
-    private String updatedDate;
-    private String birth;
+    public String userName;
+    public Integer age;
+    public String  sex;
+    public String URLImage;
 
-    public String getUserId() {
-        return userId;
+    public User(String userName, Integer age, String sex, String URLImage) {
+        this.userName = userName;
+        this.age = age;
+        this.sex = sex;
+        this.URLImage = URLImage;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(String createDate) {
-        this.createDate = createDate;
-    }
-
-    public String getUpdatedDate() {
-        return updatedDate;
-    }
-
-    public void setUpdatedDate(String updatedDate) {
-        this.updatedDate = updatedDate;
-    }
-
-    public String getBirth() {
-        return birth;
-    }
-
-    public void setBirth(String birth) {
-        this.birth = birth;
+    public User() {
     }
 }
