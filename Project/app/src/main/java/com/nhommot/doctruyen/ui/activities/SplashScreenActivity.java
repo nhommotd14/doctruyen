409b6d584dcf66f02f195e2587d047ecd83d5e4a
Krader <hiimkol@gmail.com>
2018-05-06 16:55:09 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Project/app/src/main/AndroidManifest.xml
    fix SplashScreen

---
+++ Project/app/src/main/java/com/nhommot/doctruyen/ui/activities/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.nhommot.doctruyen.ui.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+    }
+}
